83de17b1febc7bd2f618e04e95f75fad219ad0f8
ImranShah1109 <imranshaha41@gmail.com>
2023-01-18 20:04:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function threeSum(arr, target) {
   //your code here
+	arr = arr.sort();
+	let ans = 0;
+	let comp = 0;
+	for(let i=0;i<arr.length-2;i++)
+		{
+			for(let j=i+1;j<arr.length-1;j++)
+				{
+					for(let k=j+1;k<arr.length;k++)
+						{
+							ans = arr[i] + arr[j] + arr[k];
+							if(ans <= target )
+							{
+								comp = target - ans;
+								if(comp == 1) 
+									return ans;
+							}
+							else if(ans >= target)
+							{
+								comp = ans - target;
+								if(comp == 1)
+									return ans;
+							}
+							else if(ans == target) 
+								return ans;
+						}
+				}
+		}
+
+	
 }
 
 module.exports = threeSum;
